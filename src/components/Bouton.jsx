77d53cb68a60ab4137e106eab7cf02a2ec53187b
narudb <benjam.d@gmail.com>
2020-04-29 17:56:45 +0200

    change button

--- src/components/Bouton.jsx
+++ src/components/Bouton.jsx
@@ -3,9 +3,9 @@ import "./styles/hero.css";
 
 export default function Button() {
   return (
-    <>
+    <div className="container">
       <button className="button">Random!</button>
       <button className="button">Choose your destination</button>
-    </>
+    </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./styles/hero.css";
+import Button from "./Bouton";
 
 export default function Hero() {
   return (
-    <>
+    <div className="hero">
       <img className="hero" src="/images/background-01.png" alt="spaceship" />
-    </>
+      <Button />
+    </div>
   );
 }
