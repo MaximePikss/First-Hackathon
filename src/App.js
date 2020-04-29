18814688bf3894325fd29d97c8c01b7ca4924022
berenice89 <berenice.despres@gmail.com>
2020-04-29 16:17:34 +0200

    pick and show video

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from "react";
-import Wilder from "./components/Wilder";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
 
 function App() {
+
+
+const [ windy, setWindy ] = useState("");
+
+const destinationId = windy
+const country = "AU"
+const url = `https://webcams.windy.com/webcams/public/embed/player/${destinationId}/day`
+
+
+useEffect(() => {
+  getWindy();
+}, []);
+
+const getWindy = () => {
+  axios
+    .get(
+      `https://api.windy.com/api/webcams/v2/list/country=${country}?key=XB2hPUk4tkDnV4Bs4HMHroBr83HawNUd`
+    )
+    .then((response) => response.data)
+    .then((data) => {
+      setWindy(data.result.webcams[0].id);
+      
+    
+  });
+}
+
+
+
+
   return (
-    <div className="App">
-      <h1>Hi there !</h1>
-      <Wilder />
+    <div>
+
+      <h1>titre</h1>
+
+<iframe src={url} title="cam"/>
+
+
     </div>
   );
 }
