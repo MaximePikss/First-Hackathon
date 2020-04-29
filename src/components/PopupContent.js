b7c8f4a6afef510369372f58f4024c20619728ab
llaulier <lioraloche@yahoo.fr>
2020-04-29 23:34:26 +0200

    customize pop up

--- src/components/PopupContent.js
+++ src/components/PopupContent.js
@@ -7,15 +7,21 @@ export default ({ close }) => (
       &times;
     </a>
     <div className="header"> Welcome !! </div>
+    <img
+      src="/images/cosmonaute.jpg"
+      alt="Meeting between an alien and an astronaute"
+    />
     <div className="content">
       {" "}
       Hi traveler! Here is my home sweet home and secondarily my spaceship. As
       my guest, you can choose our next destination. But only on Earth. Sorry
-      little human, you can go anywhere than your blue planet. (otherwise you
+      little human, you can't go anywhere but your blue planet. (otherwise you
       die)
       <br />
       With these two buttons (on bottom), you can pick a destination. You can
       choose a country or just let the random makes you a surprise :)
     </div>
+    <button>Random !</button>
+    <button> Choose my destination</button>
   </div>
 );
