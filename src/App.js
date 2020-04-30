8772e12661600e18e1d9f830c5c4a6435c8c0421
llaulier <lioraloche@yahoo.fr>
2020-04-30 09:14:00 +0200

    webcam works and opens on a new video

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { useState, useEffect } from "react";
-import Countries from './components/Countries';
-import axios from 'axios';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Countries from "./components/Countries";
+import axios from "axios";
+//import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Hero from "./components/Hero";
-import Country from './components/Country'
+//import Country from "./components/Country";
 import "./components/styles/hero.css";
 import "./App.css";
+import Webcam from "./components/Webcam";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [webcams, setWebcams] = useState([]);
 
-  const [ countries, setCountries ] = useState([]);
-  const [ webcams, setWebcams ] = useState([]);
-
-  
   // Declared 2 const : one the the ID and the other for the country
-  const destinationId = countries.id
-  const country = "AU"
-  // Then we get the url for the iframe with the third const 
+  const destinationId = countries.id;
+  const country = "AU";
+  // Then we get the url for the iframe with the third const
   //const url = `https://webcams.windy.com/webcams/public/embed/player/${destinationId}/day`
-  
 
   // in the useEffect, we get the cam's ID to use in the URL
   useEffect(() => {
@@ -36,12 +34,10 @@ function App() {
       .then((response) => response.data)
       .then((data) => {
         setCountries(data.result.countries);
-        
-        
-    });
-  }
+      });
+  };
 
-  const getWebcams = () =>{
+  const getWebcams = () => {
     axios
       .get(
         "https://api.windy.com/api/webcams/v2/list?show=webcams:image,location,player&key=XB2hPUk4tkDnV4Bs4HMHroBr83HawNUd"
@@ -49,28 +45,29 @@ function App() {
       .then((response) => response.data)
       .then((data) => {
         setWebcams(data.webcams);
-        
-        //webcams.location.country_code
-  });
-  }
-  
-  return (
-    <div>
-      <BrowserRouter>
-        <Route exact path="/">
-          <Hero />
-        </Route>
-        <Route exact path="/list">
-          <Countries countries={countries}/>
-        </Route>
-        <Route path="/webcam">
-          <Country webcams={webcams}/>
-        </Route>
-      </BrowserRouter>
-     
-     
-    </div>
 
+        //webcams.location.country_code
+      });
+  };
+
+  return (
+    <div className="container">
+      <Webcam />
+      <Hero />
+      {/*<div>
+        <BrowserRouter>
+          <Route exact path="/">
+            <Hero />
+          </Route>
+          <Route exact path="/list">
+            <Countries countries={countries} />
+          </Route>
+          <Route path="/webcam">
+            <Country webcams={webcams} />
+          </Route>
+        </BrowserRouter>
+      </div>*/}
+    </div>
   );
 }
 
