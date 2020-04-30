38269eba142c3d4f8d0b3e743689c62741b7a964
llaulier <lioraloche@yahoo.fr>
2020-04-30 13:24:24 +0200

    popup is ok

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./styles/NavBar.css";
 import Webcam from "./Webcam";
+import Popup from "reactjs-popup";
+import PopupContent from "./PopupContent";
+import "./styles/popup.css";
 
 export default function NavBar() {
   return (
     <div className="buttonclass">
-      <Link to="/list">
+      <label>Random destination!</label>
+      <Link to="/">
         <button className="button" onClick="Webcam" title="Random!"></button>
       </Link>
-      <label>Random destination!</label>
+      <label>Your guest</label>
+      <Popup
+        modal
+        trigger={<button title="YourGuest" className="button"></button>}
+      >
+        {(close) => <PopupContent close={close} />}
+      </Popup>
     </div>
   );
 }
